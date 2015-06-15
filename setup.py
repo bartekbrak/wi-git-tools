6176cc35575522279e8b29ce8f2a6439be3551dc
Bartek Rychlicki <bartek.r@webinterpret.com>
2015-06-15 16:01:20 +0200

    Bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='wi-prune-branches',
-    version='0.0.0',
+    version='1.0.0',
     py_modules=['prune_branches'],
     install_requires=['GitPython', 'blessings'],
     entry_points={'console_scripts': ['prune_branches=prune_branches:main']}
