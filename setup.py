7644a60d7c61160ae5764f09beb175be10c3063b
Bartek Rychlicki <bartek.r@webinterpret.com>
2015-06-16 08:43:05 +0200

    Add missing git_common.py.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='wi-git-tools',
     version='1.1.0',
-    py_modules=['prune_branches', 'authors'],
+    py_modules=['prune_branches', 'authors', 'git_common'],
     install_requires=['GitPython', 'blessings'],
     entry_points={'console_scripts': [
         'prune_branches=prune_branches:main',
