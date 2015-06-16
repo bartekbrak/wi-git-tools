e2d98d0d4be4089c6220cd96d07ea67b2c1c7a88
Bartek Rychlicki <bartek.r@webinterpret.com>
2015-06-16 08:41:37 +0200

    Bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='wi-git-tools',
-    version='1.0.0',
+    version='1.1.0',
     py_modules=['prune_branches', 'authors'],
     install_requires=['GitPython', 'blessings'],
     entry_points={'console_scripts': [
